c2f6625106c089c6672e9ffa0c2ce0e642b82866
alsanusi <malkautsars@gmail.com>
2020-09-25 15:18:33 +0800

    feat: db_config

---
+++ db-config.js
@@ -0,0 +1,15 @@
+let config = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        port: 3306,
+        database: 'belajar_express'
+    },
+    server: {
+        host: '127.0.0.1',
+        port: '3003'
+    }
+}
+
+module.exports = config
