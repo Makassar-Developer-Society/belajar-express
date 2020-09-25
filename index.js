7e200e2bc02e25f5a8f1ac79090238b5f024115c
alsanusi <malkautsars@gmail.com>
2020-09-25 15:09:47 +0800

    init: sql

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 
 // Testing API
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
     res.send('Belajar EXPRESS')
 })
 
 // Testing PORT
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
     console.log('Server running at Port:' + PORT)
 })
