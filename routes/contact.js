8fe3025ba0e8f1ea1ea6ee2bb37eef4e50b860b6
alsanusi <malkautsars@gmail.com>
2020-09-25 15:51:43 +0800

    feat: input contact

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,24 @@ const app = express()
 
 app.get('/', (req, res) => {
     req.getConnection((err, con) => {
-        con.query('SELECT * FROM tb_contact', (err, rows) => {
+        con.query('SELECT * FROM tb_contact', (err, rows, fields) => {
+            if (err) {
+                res.status(400).json(err)
+            } else {
+                res.status(200).json(rows)
+            }
+        })
+    })
+})
+
+app.post('/', (req, res) => {
+    req.getConnection((err, con) => {
+        let contactData = {
+            name: req.body.name,
+            email: req.body.email,
+            enquiry: req.body.enquiry,
+        }
+        con.query('INSERT INTO tb_contact SET ?', [contactData], (err, rows, fields) => {
             if (err) {
                 res.status(400).json(err)
             } else {
